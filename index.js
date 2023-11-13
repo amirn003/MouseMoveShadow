cfca3ff139bbcf103fd2bfb5ba05ad17027801b9
amirn003 <amir.naar@gmail.com>
2023-11-13 14:18:14 +0100

    catch x and y coordinates

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
+
+
+function shadow(e) {
+  const width = hero.offsetWidth;
+  const height = hero.offsetHeight;
+
+  let {offsetX: x, offsetY: y} = e;
+
+  console.log(x, y);
+}
+
+hero.addEventListener('mousemove', shadow);
